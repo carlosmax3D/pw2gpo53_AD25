b5575b207f9f294e257f3a0ffbc1cbd7707ce53b
Carlos Israel Orta Orta <carlos.orta@coreapps.mx>
2025-11-07 11:36:07 -0600

    Added proxy and fixed call to backend

--- client/src/PageUsuario.jsx
+++ client/src/PageUsuario.jsx
@@ -19,7 +19,7 @@ export default function  PageUsuario()  {
         setInputValue(event.target.value);
     };
   useEffect(() => {
-    axios.get('https://pw2gpo53-ad-25.vercel.app/api/users/users')
+    axios.get('/api/users/users')
       .then(response => {
         setData(response.data.data);
       })
